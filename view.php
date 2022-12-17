36dd5ca302756cc0f297c822d0006ef11e7568f4
rizwanrn <94157738+rizwanrn@users.noreply.github.com>
2022-12-17 15:10:00 +0530

    Create view.php
    Put this file which is going to create a pdf and save to server

---
+++ view.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<head>
+<title>Certificate</title>    
+<style>
+body {
+  background-image:  url(https://chitranjali.net/assest/img/Chitranjali certificate-03.png);
+    background-size:   contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+}
+@page {
+  size: A4;
+  margin: 0;
+}
+@media print {
+  html, body {
+    width: 210mm;
+    height: 297mm;
+  }
+}
+</style>
+</head>
+<body>
+</body>
+</html>
